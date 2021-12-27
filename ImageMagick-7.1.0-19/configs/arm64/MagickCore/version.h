88e7013ea2376ccc6e76d335ea9350165959bc91
GitHub <noreply@github.com>
2021-12-27 18:27:07 +0000

    Update ImageMagick from 7.1.0-18 -> 7.1.0-19

--- ImageMagick-7.1.0-19/configs/arm64/MagickCore/version.h
+++ ImageMagick-7.1.0-19/configs/arm64/MagickCore/version.h
@@ -30,16 +30,16 @@ extern "C" {
 #define MagickLibVersion  0x710
 #define MagickLibVersionText  "7.1.0"
 #define MagickLibVersionNumber  10,0,0
-#define MagickLibAddendum  "-18"
+#define MagickLibAddendum  "-19"
 #define MagickLibInterface  10
 #define MagickLibMinInterface  10
 #define MagickPlatform  "aarch64"
 #define MagickppLibVersionText  "7.1.0"
 #define MagickppLibVersionNumber  5:0:0
-#define MagickppLibAddendum  "-18"
+#define MagickppLibAddendum  "-19"
 #define MagickppLibInterface  5
 #define MagickppLibMinInterface  5
-#define MagickReleaseDate  "2021-12-04"
+#define MagickReleaseDate  "2021-12-18"
 #define MagickAuthoritativeLicense  \
   "https://imagemagick.org/script/license.php"
 #define MagickAuthoritativeURL  "https://imagemagick.org"
