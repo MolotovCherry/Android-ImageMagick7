2c6d3f363446d1898b65d73ead48e66d6998a39c
GitHub <noreply@github.com>
2022-03-27 18:35:38 +0000

    Update ImageMagick from 7.1.0-28 -> 7.1.0-29

--- ImageMagick-7.1.0-29/MagickCore/quantum.c
+++ ImageMagick-7.1.0-29/MagickCore/quantum.c
@@ -689,13 +689,21 @@ MagickExport MagickBooleanType SetQuantumDepth(const Image *image,
           else
             quantum_info->depth=16;
     }
-  if (quantum_info->pixels != (MemoryInfo **) NULL)
-    DestroyQuantumPixels(quantum_info);
-  quantum=(quantum_info->pad+MaxPixelChannels)*(quantum_info->depth+7)/8;
+  /*
+    Speculative allocation since we don't yet know the quantum type.
+  */
+  quantum=(GetPixelChannels(image)+quantum_info->pad+3)*
+    ((quantum_info->depth+7)/8)*sizeof(double);
   extent=MagickMax(image->columns,image->rows)*quantum;
   if ((MagickMax(image->columns,image->rows) != 0) &&
       (quantum != (extent/MagickMax(image->columns,image->rows))))
     return(MagickFalse);
+  if (quantum_info->pixels != (MemoryInfo **) NULL)
+    {
+      if (extent <= quantum_info->extent)
+        return(MagickTrue);
+      DestroyQuantumPixels(quantum_info);
+    }
   return(AcquireQuantumPixels(quantum_info,extent));
 }
 
@@ -967,8 +975,7 @@ MagickExport void SetQuantumMinIsWhite(QuantumInfo *quantum_info,
 %
 %  The format of the SetQuantumQuantum method is:
 %
-%      void SetQuantumQuantum(QuantumInfo *quantum_info,
-%        const size_t quantum)
+%      void SetQuantumQuantum(QuantumInfo *quantum_info,const size_t quantum)
 %
 %  A description of each parameter follows:
 %
