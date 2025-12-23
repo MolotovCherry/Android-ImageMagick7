851cbbf937c959e54df97192f09d28e392e7953e
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2025-12-23 08:05:29 -0800

    Remove download option from docker script
    Removed conditional logic for image handling and simplified the build process.

--- build-with-docker.bat
+++ build-with-docker.bat
@@ -1,25 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
-for /f "delims=" %%i in ('docker images -q cherryleafroad/imbuild') DO set imbuild=%%i
-
-if [!imbuild!]==[] (
-  set /p choice="Download(d) or build(b) image? [dB]: "
-  
-  if "!choice!"=="d" (
-    docker pull cherryleafroad/imbuild
-    goto continue
-  ) else (
-    if "!choice!"=="b" (
-      docker build %~dp0 --tag cherryleafroad/imbuild
-      goto continue
-    )
-  )
-  
-  docker build %~dp0 --tag cherryleafroad/imbuild
-)
-
-:continue
+docker build %~dp0 --tag cherryleafroad/imbuild
 
 for /f "delims=" %i in ('"docker volume inspect imbuild 2>NUL"') DO set imvolume=%i
 
